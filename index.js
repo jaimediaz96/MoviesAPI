0c9b0862a4a6b7562778ab4ba0bdd6c7e191341b
JaimeDiaz <jedt1996@gmail.com>
2023-07-05 12:20:16 -0500

    Connection with MongoDB

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// body parser
+app.use(express.json());
 
-app.get('/json', (req, res) => {
-  res.json({ hello: 'world' });
-});
+moviesApi(app);
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
